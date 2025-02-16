8c180980fc6dd7fbc62d9e5de398b05aa4ed4eb3
yelouu <supercomboultrasamurai@gmail.com>
2025-02-16 23:30:00 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,26 +1,2 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    //1
-  let str = "abcde";
-    alert(str[0]);
-    alert(str[1]);
-    alert(str[2]);
-    alert(str[3]);
-    alert(str[4]);
-
-  
-    //2
-    let a = prompt("Введите первое число:");
-     let b = prompt("Введите второе число:");
-    console.log(a > 0 || b > 0);
-  
-    //3
-  
-    let  age = prompt("Введите ваш возарст:");
-    if (age >= 18)
-      {console.log("Совершеннолетний")}
-
-    else 
-      {console.log("Несовершеннолетний")};
+*не то*
   });
